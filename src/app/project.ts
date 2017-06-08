8d38e27cb6d51ce303eb49b0ad33ee70dea85da7
Reilly Wood <reilly.wood@icloud.com>
2017-06-08 20:28:08 +0200

    emailaddress in config

--- src/app/project.ts
+++ src/app/project.ts
@@ -1,6 +1,8 @@
 export class Project {
+	constructor(id: string, name: string, emailToAddress: string, neighbourhood: string, description: string, tags: string[]){
 		this.id = id;
 		this.name = name;
+		this.emailToAddress = emailToAddress;
 		this.neighbourhood = neighbourhood;
 		this.description = description;
 		this.tags = tags;
@@ -8,6 +10,7 @@ export class Project {
 
     id: string;
     name: string;
+    emailToAddress: string;
     neighbourhood: string;
     description: string;
     tags: string[];
