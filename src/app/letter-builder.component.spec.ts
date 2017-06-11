4532c74d6b3e7ae9f6b4917d9526baaae19942b9
Reilly Wood <reilly.wood@icloud.com>
2017-06-11 20:07:12 +0200

    Note that (default) tests aren't working

--- src/app/letter-builder.component.spec.ts
+++ src/app/letter-builder.component.spec.ts
@@ -1,30 +1,30 @@
 import { TestBed, async } from '@angular/core/testing';
+import { LetterBuilderComponent } from './letter-builder.component';
+import 'rxjs/Rx';
 
-import { AppComponent } from './letter-builder.component';
-
-describe('AppComponent', () => {
+//TODO: these tests are basically just the default ones and they don't even work now. Worth creating real tests someday! 
+describe('LetterBuilderComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        LetterBuilderComponent
       ],
     }).compileComponents();
   }));
-
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(LetterBuilderComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
   it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(LetterBuilderComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app works!');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(LetterBuilderComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('app works!');
