bb7f296b53fe03fe5ed9543e61f6691ae338cee2
Reilly Wood <reilly.wood@icloud.com>
2017-07-11 20:17:17 +0200

    Disable send button after sending, more polyfill

--- src/app/letter-builder.component.ts
+++ src/app/letter-builder.component.ts
@@ -30,6 +30,8 @@ export class LetterBuilderComponent {
   letterSubject: string = '';
   letterBody: string = '';
   dataLoaded: boolean = false;
+  sendLetterButtonText: string = 'Send Letter';
+  letterSent: boolean = false;
 
   ngOnInit(): void {
     this.dataService.init(() => {
@@ -165,6 +167,59 @@ export class LetterBuilderComponent {
   }
 
   getQueryParams(qs: string) : {} {
+    //polyfill for IE
+    // https://tc39.github.io/ecma262/#sec-array.prototype.includes
+    if (!Array.prototype.includes) {
+      Object.defineProperty(Array.prototype, 'includes', {
+        value: function(searchElement, fromIndex) {
+    
+          // 1. Let O be ? ToObject(this value).
+          if (this == null) {
+            throw new TypeError('"this" is null or not defined');
+          }
+    
+          var o = Object(this);
+    
+          // 2. Let len be ? ToLength(? Get(O, "length")).
+          var len = o.length >>> 0;
+    
+          // 3. If len is 0, return false.
+          if (len === 0) {
+            return false;
+          }
+    
+          // 4. Let n be ? ToInteger(fromIndex).
+          //    (If fromIndex is undefined, this step produces the value 0.)
+          var n = fromIndex | 0;
+    
+          // 5. If n ≥ 0, then
+          //  a. Let k be n.
+          // 6. Else n < 0,
+          //  a. Let k be len + n.
+          //  b. If k < 0, let k be 0.
+          var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+    
+          function sameValueZero(x, y) {
+            return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+          }
+    
+          // 7. Repeat, while k < len
+          while (k < len) {
+            // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+            // b. If SameValueZero(searchElement, elementK) is true, return true.
+            // c. Increase k by 1. 
+            if (sameValueZero(o[k], searchElement)) {
+              return true;
+            }
+            k++;
+          }
+    
+          // 8. Return false
+          return false;
+        }
+      });
+    }
+
     qs = qs.split('+').join(' ');
 
     var params = {},
@@ -194,13 +249,13 @@ export class LetterBuilderComponent {
   }
 
   readyToSendLetter(): boolean{
-    if(this.letterSubject && this.letterBody && this.letterSubject.trim() != '' && this.letterBody.trim() != '')
+    if(!this.letterSent && this.letterSubject && this.letterBody && this.letterSubject.trim() != '' && this.letterBody.trim() != '')
       return true;
     else
       return false;
   }
 
-  sendLetter(): void{  
+  sendLetter(): void{
     console.log('Posting letter')
     let url = "https://8zp8hsoa63.execute-api.us-west-2.amazonaws.com/prod/submit";
     let data = {};
@@ -213,5 +268,7 @@ export class LetterBuilderComponent {
     .subscribe( () => {},
       err => {console.log('Error when posting'); console.log(err)},
       () => console.log('Post complete'));
+      this.letterSent = true;
+      this.sendLetterButtonText = "Letter sent";
   }
 }
