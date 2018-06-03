4d1eb9fe7f9590af4940f4d02ff23bfa43929eeb
Brad Cavanagh <brad.cavanagh@gmail.com>
2018-06-03 14:19:30 -0700

    Add address to project constructor.

--- src/app/project.ts
+++ src/app/project.ts
@@ -7,12 +7,13 @@ export class Project {
 	address: string;
     tags: string[];
 
-    constructor(id: string, name: string, emailToAddresses: string[], neighbourhood: string, description: string, tags: string[]){
+    constructor(id: string, name: string, emailToAddresses: string[], neighbourhood: string, description: string, address: string, tags: string[]){
         this.id = id;
         this.name = name;
         this.emailToAddresses = emailToAddresses;
         this.neighbourhood = neighbourhood;
         this.description = description;
+        this.address = address;
         this.tags = tags;
     }
 }
