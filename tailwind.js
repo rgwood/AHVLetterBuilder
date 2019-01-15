47654407138a0033168fd4834bd4897943589674
Reilly <reilly.wood@icloud.com>
2019-01-14 20:37:00 -0800

    Tweak tailwind.js to keep CSS size down

--- tailwind.js
+++ tailwind.js
@@ -58,7 +58,7 @@ let colors = {
   'grey-darkest': '#3d4852',
   // 'grey-darker': '#606f7b',
   // 'grey-dark': '#8795a1',
-  // 'grey': '#b8c2cc',
+  'grey': '#b8c2cc',
   'grey-light': '#dae1e7',
   // 'grey-lighter': '#f1f5f8',
   // 'grey-lightest': '#f8fafc',
